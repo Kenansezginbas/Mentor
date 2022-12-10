a587c4efe7484ee77a1f01d18f479d7f4e3a046c
KenanSezginbas <31697908+Kenansezginbas@users.noreply.github.com>
2022-12-10 21:44:34 +0300

    Add JsonSerializable

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mentor/controller/tab_bar_controller.dart';
 import 'package:mentor/firebase_options.dart';
 import 'package:mentor/utils/custom_colors.dart';
 import 'package:mentor/view/add_question.dart';
 import 'package:mentor/view/auth/sign_in_view.dart';
 import 'package:mentor/view/auth/sign_up_view.dart';
+import 'package:mentor/view/controller/tab_bar_controller.dart';
 import 'package:mentor/view/home_view.dart';
 import 'package:mentor/view/menu_view.dart';
 import 'package:mentor/view/question_answer_view.dart';
--- lib/model/blog_model.dart
+++ lib/model/blog_model.dart
@@ -1,3 +1,6 @@
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
 class BlogModel {
   String? blogTitle;
   String? blogDate;
@@ -7,21 +10,6 @@ class BlogModel {
 
   BlogModel({this.blogTitle, this.blogDate, this.blogContent, this.blogWriter});
 
-  BlogModel.fromJson(Map<String, dynamic> json) {
-    blogTitle = json['blogTitle'];
-    blogDate = json['blogDate'];
-    blogContent = json['blogContent'];
-    blogWriter = json['blogWriter'];
-    blogUrl = json['blogUrl'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['blogTitle'] = blogTitle;
-    data['blogDate'] = blogDate;
-    data['blogContent'] = blogContent;
-    data['blogWriter'] = blogWriter;
-    data['blogUrl'] = blogUrl;
-    return data;
-  }
+  factory BlogModel.fromJson(Map<String, dynamic> json) => _$BlogFromJson(json);
+  Map<String, dynamic> toJson() => _$BlogModelToJson(this);
 }
